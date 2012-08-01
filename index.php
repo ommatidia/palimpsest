b476017e04bc53b038d6d27132a78aee1c00d789
Ommatidia <ommatidia@gmail.com>
2012-07-31 21:00:32 -0400

    updated index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+
+  <link rel="stylesheet" href="styles.css"></link>
+
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+  <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyB8wuSJdYBYyYXHH8x8wTHyS5vnX8Xtyvo&sensor=true" type="text/javascript"></script>
+
+  <script src="pages.js" type="text/javascript"></script>
+  <script src="index.js" type="text/javascript"></script>
+
+</head>
+<body>
+  <div id="menubar" class="menubar">
+    <div class="title">The Archimedes Palimpsest</div>
+    <div class="spacer">
+      <button id="ppage">Prev Page</button>
+      <button id="npage">Next Page</button>
+      <button id="prev">Prev Image</button>
+      <button id="next">Next Image</button>
+    </div>
+  </div>
+  <div id="gmaps_container" class="map"></div>
+</body>
+</html>
