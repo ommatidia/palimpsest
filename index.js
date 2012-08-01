6f7201e3720fa76cd0de94f744e60b9a1013f0b6
Ommatidia <ommatidia@gmail.com>
2012-07-31 21:01:49 -0400

    updated index.js to create useful gmaps interface

--- index.js
+++ index.js
@@ -1,9 +1,140 @@
-
 $(window).ready(function() {
-    //temp();
-    //initialize();
+    registerListeners();
+    
+    resize();
+    $(window).bind("resize", resize);
     imageType();
 });
+
+
+function registerListeners() {
+    
+    $('#prev').bind('click', function(evt) {
+	var current = currentPage();
+	var p = current['page'];
+	var v = current['view'];
+	if(v == "1") {
+	    var p = pages[p]['_prev'];
+	    if(!p) {
+		return;
+	    }
+	    
+	    v = pages[p]['_length'];
+	} else {
+	    v = ""+(Number(view)-1)
+	}
+	currentPage(p, v);
+    });
+
+    $('#next').bind('click', function(evt) {
+	var current = currentPage();
+	var p = current['page'];
+	var v = current['view'];
+	if(v == pages[p]['_length']) {
+	    v = "1";
+	    p = pages[p]['_next']
+	    if(!p) {
+		return;
+	    }
+	} else {
+	    v = ""+(Number(v) + 1);
+	}
+	currentPage(p, v);
+    });
+
+    $('#ppage').bind('click', function(evt) {
+	prevPage();
+    });
+
+    $('#npage').bind('click', function(evt) {
+	nextPage();
+    });
+}
+
+function prevPage() {
+    var current = currentPage();
+    var page = current['page']
+    var prev = pages[page]['_prev'];
+    if(prev) {
+	currentPage(prev, "1");
+    }
+}
+
+function nextPage() {
+    var current = currentPage();
+    var page = current['page'];
+    var next = pages[page]['_next'];
+    if(next) {
+	currentPage(next, "1");
+    }
+}
+
+function currentPage(page, view) {
+    if(page && view) {
+	//ensure valid
+	this.page = page;
+	this.view = view;
+	loadPage(this.page, this.view);
+    }
+
+    if(!this.page) {
+	this.page = "0000-088v";
+    }
+    if(!this.view) {
+	this.view = "1";
+    }
+
+    return {
+	'page': this.page,
+	'view': this.view
+    };
+	
+}
+
+
+function resize() {
+    var body = $('body');
+    var margins = body.outerHeight(true) - body.height();
+    $('#gmaps_container').height(
+	window.innerHeight-$('.menubar').outerHeight(true)-margins
+    );
+}
+
+function getMapTypeIds() {
+    return [
+	'palimpsest',
+	'palimpsest2'
+    ]
+}
+
+function reloadTiles() {
+    if(!this.current) {
+	this.current = 0;
+    }
+    this.current = (this.current + 1) % 2;
+    var id = getMapTypeIds()[this.current];
+    if(getMap().mapTypes.hasOwnProperty(id)) {
+	getMap().setMapTypeId(id);
+    }
+}
+
+
+function getImage(image) {
+    if(image) {
+	this.image = image;
+    }
+    if(!this.image) {
+	var page = this.currentPage();
+	this.image = page['page'] + "/" + page['view'];
+    }
+    return this.image;
+}
+
+function loadPage(page, view) {
+    getImage(page + "/" + view);
+    reloadTiles();
+}
+
 
 function imageType() {
     var imageTypeOptions = {
@@ -16,16 +147,19 @@ function imageType() {
 	    var bound = Math.pow(2, zoom);
 	    //var base = "http://66.92.65.8:8080/palimpsest/imgs/";
 	    //return base + "${zoom}/$
-	    return "http://mw1.google.com/mw-planetary/lunar/lunarmaps_v1/clem_bw/" + zoom + "/" + normalizedCoord.x + "/" + (bound - normalizedCoord.y - 1) + ".jpg";
+	    //return "http://mw1.google.com/mw-planetary/lunar/lunarmaps_v1/clem_bw/" + zoom + "/" + normalizedCoord.x + "/" + (bound - normalizedCoord.y - 1) + ".jpg";
+	    
+	    return "http://66.92.65.8:8080/palimpsest/tiles/" + getImage() + "/level_%{zoom}/%{x}_%{y}.png".replace(/%{zoom}/g, zoom).replace(/%{x}/g, normalizedCoord.x).replace(/%{y}/g, (normalizedCoord.y));
 	},
-	tileSize: new google.maps.Size(256, 256),
-	maxZoom: 9,
+	tileSize: new google.maps.Size(128, 171),
+	maxZoom: 5,
 	minZoom: 0,
-	radius: 1738000,
-	name: "Moon"
+	radius: 10,
+	name: "Archimedes Palimpsest"
     };
 
     var moonMapType = new google.maps.ImageMapType(imageTypeOptions);
+    var altMapType = new google.maps.ImageMapType(imageTypeOptions);
 
     var myLatlng = new google.maps.LatLng(0,0);
     var mapOptions = {
@@ -33,13 +167,21 @@ function imageType() {
 	zoom: 1,
 	streetViewControl: false,
 	mapTypeControlOptions: {
-	    mapTypeIds: ["moon"]
+	    mapTypeIds: ["palimpsest"]
 	}
     };
 
-    var map = new google.maps.Map(document.getElementById("gmaps_container"), mapOptions);
-    map.mapTypes.set('moon', moonMapType);
-    map.setMapTypeId('moon');
+    this.getMap(mapOptions, moonMapType, altMapType);
+}
+
+function getMap(mapOptions, moonMapType, altMapType) {
+    if(!this.map) {
+	this.map = new google.maps.Map(document.getElementById("gmaps_container"), mapOptions);
+	this.map.mapTypes.set('palimpsest', moonMapType);
+	this.map.mapTypes.set('palimpsest2', altMapType);
+	this.reloadTiles(); //setMapId
+    }
+   return this.map;
 }
 
 function getNormalizedCoord(coord, zoom) {
@@ -61,26 +203,5 @@ function getNormalizedCoord(coord, zoom) {
     };
 }
 
-var ARCHEMEDES_MAP_TYPE = 'archemedes';
 
-function temp() {
 
-    var options = {
-	/*mapTypeControlOptions: {
-	    mapTypeIds: [ARCHEMEDES_MAP_TYPE]
-	},
-	mapTypeId: ARCHEMEDES_MAP_TYPE,*/
-	mapTypeId: google.maps.MapTypeId.ROADMAP,
-	zoom: 1,
-	center: new google.maps.LatLng(0.0, 0.0)
-    };
-    var map = new google.maps.Map(document.getElementById("gmaps_container"), options);
-}
-
-function initialize() {
-    map = new GMaps({
-	div: '#gmaps_container',
-	lat: -12.04,
-	lng: -77.02
-    });
-}
