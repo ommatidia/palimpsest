48887e7ab8adfea97e9153a58d36e868d00e9f6a
Ommatidia <ommatidia@gmail.com>
2012-09-05 19:49:01 -0400

    removed prototype code, fixed whitespace/readability

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" href="static/css/jquery.thumbnailScroller.css"></link>
     <link rel="stylesheet" href="static/css/styles.css"></link>
     
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
     <script src="static/js/jquery-ui-1.8.13.custom.min.js" type="text/javascript"></script>
     <script src="static/js/jquery.thumbnailScroller.js" type="text/javascript"></script>
 
@@ -12,7 +12,6 @@
        $key = getenv('GMAPS_API_KEY');
        printf("<script src='http://maps.googleapis.com/maps/api/js?key=%s&sensor=true' type='text/javascript'></script>", $key);
     ?>
-
 
     <script src="pages.js" type="text/javascript"></script>
     <script src="index.js" type="text/javascript"></script>
@@ -28,6 +27,7 @@
 	<button id="next">Next Layer</button>
       </div>
     </div>
+
     <div id="gmaps_container" class="map"></div>
     <div class="rpanel"></div>
     
@@ -38,36 +38,6 @@
       <a href="#" class="jTscrollerPrevButton"></a>
       <a href="#" class="jTscrollerNextButton"></a>
     </div>
-    <script>/*
-      window.onload = function() {
-      function createThumbnailLink(pageId, pageNum) {
-      img = document.createElement('img');
-      img.setAttribute('src', 'tiles/' + pageId + '/1/level_0/0_0.png');
-      img.setAttribute('title', 'Page ' + pageNum);
-           img.setAttribute('alt', 'Page ' + pageNum);
-           
-           
-           anchor = $('<a/>', { 'href': '#' });
-           anchor.bind('click', function() {
-               currentPage(pageId, "1");
-           });
-           anchor.append(img);
-           return anchor;
-       }
-       var num = 1;
-       for(key in pages) {
-          x = createThumbnailLink(key, num);
-          num++;
-          $('.jTscroller').append(x);
-       }
-       $('#ts2_container').thumbnailScroller({
-           noScrollCenterSpace: 400,
-           scrollerType: 'clickButtons',
-           acceleration: 2,
-           scrollSpeed: 800
-       });
-    };*/
-  </script>
+  </body>
 
-</body>
 </html>
